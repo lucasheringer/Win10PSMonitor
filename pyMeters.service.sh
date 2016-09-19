0140daea7bb41f4b3c5494397c1f167131f579ae
Lucas Caldas Heringer de Freitas <lucasheringer@gmail.com>
2016-09-19 10:51:02 -0300

    modified:   pyMeters.service.sh

--- pyMeters.service.sh
+++ pyMeters.service.sh
@@ -4,19 +4,19 @@
 #Script to control pymeters process stopping and starting correctly
 
 PID=`ps -ef | grep pyMeters | awk '{print $2}'`
-COUNT=`cat $PID | wc -l`
+COUNT=`echo $PID | wc -l`
 
-if $COUNT > 1
+if ( $COUNT > 1 )
   then
     for i in PID;
       do
         echo "killing process: " $i
-        sudo kill -9 $i
+        kill -9 $i
       done
 else
     echo "No process to kill"
 fi
 
-sudo python pyMetersWin10.py &
+python pyMetersWin10.py &
 
 exit 0
