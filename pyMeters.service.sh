0acd93173d9dbf3160f7ca7c2a4724fca8f3667f
Lucas Caldas Heringer de Freitas <lucasheringer@gmail.com>
2016-09-19 15:07:08 -0300

    modified:   pyMeters.service.sh

--- pyMeters.service.sh
+++ pyMeters.service.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 #Script to control pymeters process stopping and starting correctly
+#python /home/pi/pyMetersWin10.py >/dev/null
 
-PID=`ps -ef | grep python | awk '{print $2}'|head -n -1`
-for i in $PID;
-  do
-    echo "killing process: " $i
-    kill -9 $i
-done
+case "$1" in
+start)
+   python /home/pi/pyMetersWin10.py &
+   echo $!>/var/run/pyMeters.pid
+   ;;
+stop)
+   kill `cat /var/run/hit.pid`
+   rm /var/run/pyMeters.pid
+   ;;
+restart)
+   $0 stop
+   $0 start
+   ;;
+status)
+   if [ -e /var/run/pyMeters.pid ]; then
+      echo pyMetersWin10.py is running, pid=`cat /var/run/pyMeters.pid`
+   else
+      echo pyMetersWin10.py is NOT running
+      exit 1
+   fi
+   ;;
+*)
+   echo "Usage: $0 {start|stop|status|restart}"
+esac
 
-echo "starting pyMetersWin10"
-
-sleep 1
-
-python /home/pi/pyMetersWin10.py >/dev/null
-
-echo "is it working?"
+exit 0
