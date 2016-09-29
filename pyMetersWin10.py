45a53d51ae6136c262fc262fe6dea8ace4eaa9cf
Lucas Caldas Heringer de Freitas <lucasheringer@gmail.com>
2016-09-29 20:40:43 -0300

    modified:   pyMetersWin10.py

--- pyMetersWin10.py
+++ pyMetersWin10.py
@@ -8,7 +8,7 @@ import time
 import subprocess
 
 FPS = 5
-newUrl = "http://192.168.1.209:8085/data.json"
+newUrl = "http://192.168.1.90:8085/data.json"
 
 # Define basic colors
 SHADOW = (192, 192, 192)
